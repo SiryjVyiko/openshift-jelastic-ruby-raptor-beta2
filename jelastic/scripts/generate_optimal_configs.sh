98c4f2e992089fd6b6676adf4c21afabc5d165a7
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2015-01-12 16:01:26 +0200

    Update generate_optimal_configs.sh

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-. /etc/environment
+source /etc/environment 
 
 version=`passenger-config --version`
 
 if [[ $version =~ 4.* ]] ;then
 
-    gem install passenger -v 5.0.0.beta1 >>/opt/repo/log/out.log 2>&1 ;
+    gem install passenger -v 5.0.0.beta2 >>/opt/repo/log/out.log 2>&1 ;
 
     gem uninstall passenger -v 4.0.50 >>/opt/repo/log/out.log 2>&1 ;
 
@@ -15,5 +15,7 @@ if [[ $version =~ 4.* ]] ;then
     passenger-config install-standalone-runtime >>/opt/repo/log/out.log 2>&1 ;
 
     passenger-config build-native-support >>/opt/repo/log/out.log 2>&1 ;
+    
+    [ -f $GEM_HOME/gems/passenger-5.0.0.beta2/lib/phusion_passenger/standalone/start_command/builtin_engine.rb ] && { sed -ib_q 's/ecape/escape/' $GEM_HOME/gems/passenger-5.0.0.beta2/lib/phusion_passenger/standalone/start_command/builtin_engine.rb; }
 
 fi
