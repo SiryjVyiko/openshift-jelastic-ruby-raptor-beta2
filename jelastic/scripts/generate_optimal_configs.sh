94ffeb7b02e375bb90ba3a81ccdb665e6eb68b4a
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2015-02-20 01:14:13 +0200

    Update generate_optimal_configs.sh

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -6,9 +6,9 @@ version=`passenger-config --version`
 
 if [[ $version =~ 4.* ]] ;then
 
-    gem install passenger -v 5.0.0.beta2 >>/opt/repo/log/out.log 2>&1 ;
+    gem install passenger --pre >>/opt/repo/log/out.log 2>&1 ;
 
-    gem uninstall passenger -v 4.0.50 >>/opt/repo/log/out.log 2>&1 ;
+    gem uninstall passenger -v `gem list|grep passenger|grep -o "4[.0-9]*"` >>/opt/repo/log/out.log 2>&1 ;
 
     passenger-config install-agent >>/opt/repo/log/out.log 2>&1 ;
 
@@ -16,6 +16,6 @@ if [[ $version =~ 4.* ]] ;then
 
     passenger-config build-native-support >>/opt/repo/log/out.log 2>&1 ;
     
-    [ -f $GEM_HOME/gems/passenger-5.0.0.beta2/lib/phusion_passenger/standalone/start_command/builtin_engine.rb ] && { sed -ib_q 's/ecape/escape/' $GEM_HOME/gems/passenger-5.0.0.beta2/lib/phusion_passenger/standalone/start_command/builtin_engine.rb; }
+    [ -f $GEM_HOME/gems/passenger-`gem list|grep passenger|grep -o "5[.0-9a-z]*"`/lib/phusion_passenger/standalone/start_command/builtin_engine.rb ] && { sed -ib_q 's/ecape/escape/' $GEM_HOME/gems/passenger-`gem list|grep passenger|grep -o "5[.0-9a-z]*"`/lib/phusion_passenger/standalone/start_command/builtin_engine.rb; }
 
 fi
